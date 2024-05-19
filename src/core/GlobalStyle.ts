df8997505f802303ac0c6ee72a51f3f978c4bd84
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-05-19 21:33:18 +0200

    GlobalStyle added

---
+++ src/core/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    overflow-y: scroll;
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+    word-break: break-word;
+    background-color: white;
+}
+`;
