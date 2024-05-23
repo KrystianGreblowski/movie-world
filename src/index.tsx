ed1d3069951021309040de79f9c68e219708aac1
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-05-23 20:35:09 +0200

    React Query Devtools installed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "styled-components";
 import App from "./core/App";
 import { GlobalStyle } from "./core/GlobalStyle";
@@ -14,6 +15,7 @@ const queryClient = new QueryClient();
 
 root.render(
   <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
