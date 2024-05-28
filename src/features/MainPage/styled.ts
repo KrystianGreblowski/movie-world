c78ffe9d7c407f6b32a93055332bc7e70d5b0716
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-05-28 23:42:47 +0200

    SectionWrapper, SectionTilesContainer moved to MainPage styled

--- src/features/MainPage/styled.ts
+++ src/features/MainPage/styled.ts
@@ -1,13 +1,27 @@
 import styled from "styled-components";
 
+const sectionTilesGap: number = 12;
+
 export const Wrapper = styled.main`
   max-width: 1400px;
   padding: 0 16px;
-  margin: 0 auto;
+  margin: 64px auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 64px;
 `;
 
+export const SectionWrapper = styled.section``;
+
 export const SectionTitle = styled.h2`
-  font-size: 36px;
+  font-size: 38px;
   font-weight: 600;
-  margin: 16px 0;
+  margin: 0 0 16px 0;
+`;
+
+export const SectionTilesContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: ${sectionTilesGap}px;
+  line-height: 0;
 `;
