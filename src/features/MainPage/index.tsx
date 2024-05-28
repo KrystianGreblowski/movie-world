0f43d0f09e295dbff15f5fd85cef036afb9fb83d
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-05-28 23:42:05 +0200

    PopularMovies, PopularSeries, UpcomingMovies added to MainPage

--- src/features/MainPage/index.tsx
+++ src/features/MainPage/index.tsx
@@ -1,10 +1,16 @@
-import { TrendingMovies } from "./TrendingMovies";
 import { Wrapper } from "./styled";
+import { TrendingMovies } from "./TrendingMovies";
+import { PopularMovies } from "./PopularMovies";
+import { PopularSeries } from "./PopularSeries";
+import { UpcomingMovies } from "./UpcomingMovies";
 
 export const MainPage = () => {
   return (
     <Wrapper>
       <TrendingMovies />
+      <PopularMovies />
+      <PopularSeries />
+      <UpcomingMovies />
     </Wrapper>
   );
 };
