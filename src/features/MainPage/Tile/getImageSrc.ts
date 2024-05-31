3549af4cf53502635f005bb28169e4895bd804f2
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-05-31 18:00:31 +0200

    getImageSrc created

---
+++ src/features/MainPage/Tile/getImageSrc.ts
@@ -0,0 +1,16 @@
+import placeholder from "./placeholder.png";
+import noImage from "./noImage.png";
+
+export const getImageSrc = (
+  imagePath: string,
+  imageSize: "w320" | "w780" | "w1280" | "original",
+) => {
+  switch (imagePath) {
+    case "placeholder":
+      return placeholder;
+    case "":
+      return noImage;
+    default:
+      return `https://image.tmdb.org/t/p/${imageSize}` + imagePath;
+  }
+};
