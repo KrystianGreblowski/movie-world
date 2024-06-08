8faaba3253a5bb5a07fa22c19cc7c3ab93e2fe96
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-06-09 00:19:04 +0200

    declaration for svg files added

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,5 +1,13 @@
-// src/images.d.ts
 declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
   const value: string;
   export default value;
 }
