f9dd1497979155d1313f61e0f42ecf6cfd348c7f
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-06-08 17:10:27 +0200

    side padding changed to 8px

--- src/features/MainPage/styled.ts
+++ src/features/MainPage/styled.ts
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-  max-width: 1392px;
-  padding: 0 12px;
+  max-width: 1384px;
+  padding: 0 8px;
   margin: 64px auto;
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 64px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.desktop}px) {
-    grid-gap: 5vw;
+    grid-gap: 6vw;
   }
 `;
