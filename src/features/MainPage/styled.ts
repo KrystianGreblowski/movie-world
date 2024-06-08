72848f91ead23d569d65ed27b478b1082021ed7b
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-06-08 15:51:27 +0200

    aside padding changed

--- src/features/MainPage/styled.ts
+++ src/features/MainPage/styled.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-  max-width: 1400px;
-  padding: 0 16px;
+  max-width: 1392px;
+  padding: 0 12px;
   margin: 64px auto;
   display: grid;
   grid-template-columns: 1fr;
