9bf5fdc5e97070aac95c3eaf8c28b202a6311120
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-06-17 22:52:09 +0200

    useOutsideClick added to Menu

--- src/common/Header/NavigationBar/Menu/index.tsx
+++ src/common/Header/NavigationBar/Menu/index.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { Wrapper, MenuButton, MenuImage, MenuText } from "./styled";
 import { DropdownItems } from "./DropdownItems";
+import { useOutsideClick } from "../../../useOutsideClick";
 
 export const Menu = () => {
   const [open, setOpen] = useState(false);
 
+  const ref = useOutsideClick(() => {
+    setOpen(false);
+  });
+
   return (
-    <Wrapper>
-      <MenuButton
-        onClick={() => setOpen((previousOpenState) => !previousOpenState)}
-      >
+    <Wrapper ref={ref}>
+      <MenuButton onClick={() => setOpen(!open)}>
         <MenuImage />
         <MenuText>Menu</MenuText>
       </MenuButton>
