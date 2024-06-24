0c62b4294fc1dc688ca43c90a5c878464f71c22c
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-06-24 22:11:40 +0200

    DropdownItemsProps renamed to DropdownProps

--- src/common/Header/NavigationBar/Menu/Dropdown/index.tsx
+++ src/common/Header/NavigationBar/Menu/Dropdown/index.tsx
@@ -2,17 +2,13 @@ import { DropdownItemsContainer } from "./styled";
 import { Menu } from "../index";
 import { MenuItemsData } from "../../menuItemsData";
 
-interface DropdownItemsProps {
+interface DropdownProps {
   submenus: MenuItemsData[] | undefined;
   open: boolean;
   depthLevel: number;
 }
 
-export const Dropdown = ({
-  submenus,
-  open,
-  depthLevel,
-}: DropdownItemsProps) => {
+export const Dropdown = ({ submenus, open, depthLevel }: DropdownProps) => {
   depthLevel = depthLevel + 1;
 
   return (
