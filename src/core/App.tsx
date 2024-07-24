35875b0173a540e639a299c094eeaf0beb922faf
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-07-24 20:15:22 +0200

    Scroll to top added

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../common/Header";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../common/ScrollToTop";
 
 export const App = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
     </>
