1281ab1c2759b905e3ad48e12cd2a81e51ca208e
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-07-31 17:36:41 +0200

    useDetailsDataFromApi created

---
+++ src/features/DetailsPage/useDetailsDataFromApi.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchDataFromApi } from "../../common/api/fetchDataFromApi";
+
+interface Data {
+  id: number;
+  title: string;
+  name: string;
+  genre_ids: number[];
+  backdrop_path: string;
+  poster_path: string;
+  release_date: string;
+  first_air_date: string;
+  vote_average: number;
+  vote_count: number;
+  overview: string;
+}
+
+interface useDataParameters {
+  endpoint: string;
+  params: Record<string, string>;
+}
+
+export const useDetailsDataFromApi = ({
+  endpoint,
+  params,
+}: useDataParameters) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: [endpoint, params],
+    queryFn: fetchDataFromApi<Data>,
+  });
+
+  const dataResults = data?.data || {};
+
+  return { isLoading, error, dataResults };
+};
