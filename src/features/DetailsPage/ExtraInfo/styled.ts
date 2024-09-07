3ee1b375003a6c76bf6d8a0be6486a277d74578f
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-09-07 18:33:30 +0200

    media queries added for DetailsPage ExtraInfo

--- src/features/DetailsPage/ExtraInfo/styled.ts
+++ src/features/DetailsPage/ExtraInfo/styled.ts
@@ -31,7 +31,16 @@ export const Title = styled.h1`
 export const TilesContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
-  grid-gap: 32px 16px;
+  grid-gap: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 8px;
+  }
 `;
 
 export const Tile = styled.div`
@@ -55,15 +64,45 @@ export const Info = styled.div`
   flex-wrap: wrap;
   text-align: center;
   padding: 0 12px 12px 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    text-align: center;
+    padding: 0 8px 8px 8px;
+  }
 `;
 
 export const Name = styled.p`
-  margin: 0 0 12px 0;
+  margin: 0 0 6px 0;
   font-size: 22px;
   font-weight: 600;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}px) {
+    font-size: 2.2vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    font-size: 2.6vw;
+    margin: 0 0 4px 0;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 3vw;
+  }
 `;
 export const About = styled.p`
   margin: 0;
   font-size: 16px;
   font-weight: 400;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}px) {
+    font-size: 1.6vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    font-size: 2vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 2.4vw;
+  }
 `;
