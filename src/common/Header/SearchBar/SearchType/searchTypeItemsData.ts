c6186f7225986e1269e66ec042cebdfd7b9f2500
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-09-23 22:05:07 +0200

    url added nad People deleted

--- src/common/Header/SearchBar/SearchType/searchTypeItemsData.ts
+++ src/common/Header/SearchBar/SearchType/searchTypeItemsData.ts
@@ -1,9 +1,11 @@
+import { toSearchMovies, toSearchSeries } from "../../../../core/routes";
+
 export interface SearchTypeItemsData {
   title: string;
+  url: string;
 }
 
 export const searchTypeItemsData: SearchTypeItemsData[] = [
-  { title: "Movies" },
-  { title: "Series" },
-  { title: "People" },
+  { title: "Movies", url: toSearchMovies() },
+  { title: "Series", url: toSearchSeries() },
 ];
