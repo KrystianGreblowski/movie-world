e7af8f22a904f6b99707fd73fb07198d8b1d9a35
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-09-29 20:58:04 +0200

    added one styled-component for all buttons

--- src/common/Pagination/styled.ts
+++ src/common/Pagination/styled.ts
@@ -2,6 +2,4 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div``;
 
-export const PreviousPageButton = styled.button``;
-
-export const NextPageButton = styled.button``;
+export const ChangePageButton = styled.button``;
