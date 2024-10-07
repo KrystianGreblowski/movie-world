c3fe0a6f96483807e01c2299e6bb6ed3a22364b8
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-10-07 20:43:08 +0200

    styles for wide screens for pagination added

--- src/common/Pagination/styled.ts
+++ src/common/Pagination/styled.ts
@@ -1,5 +1,36 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+`;
 
-export const ChangePageButton = styled.button``;
+export const ChangePageButton = styled.button`
+  border: none;
+  background-color: ${({ theme }) => theme.color.doveGray};
+  color: ${({ theme }) => theme.color.white};
+  padding: 6px 12px;
+  margin-right: 8px;
+  border-radius: 2px;
+  font-size: 16px;
+  font-weight: 400;
+  transition: filter 0.3s;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(1.3);
+  }
+
+  &:disabled {
+    cursor: auto;
+    filter: none;
+    background-color: ${({ theme }) => theme.color.mineShaftDark};
+  }
+`;
+
+export const PageNumberInfo = styled.p`
+  padding: 0;
+  margin: 0 8px 0 0;
+  font-size: 14px;
+`;
