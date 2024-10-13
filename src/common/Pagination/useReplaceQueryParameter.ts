71446674f4dc523b589fc3ab2f4e302269575d22
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-10-13 17:23:16 +0200

    code refactoring

--- src/common/Pagination/useReplaceQueryParameter.ts
+++ src/common/Pagination/useReplaceQueryParameter.ts
@@ -7,16 +7,10 @@ export const useReplaceQueryParameter = () => {
   return ({ url, key, value }: { url: string; key: string; value: string }) => {
     const searchParams = new URLSearchParams(location.search);
 
-    if (value === "1") {
-      searchParams.delete(key);
-      navigate(`${url}`, {
-        replace: true,
-      });
-    } else {
-      searchParams.set(key, value);
-      navigate(`${url}?${searchParams.toString()}`, {
-        replace: true,
-      });
-    }
+    value === "1" ? searchParams.delete(key) : searchParams.set(key, value);
+
+    navigate(`${url}?${searchParams.toString()}`, {
+      replace: true,
+    });
   };
 };
